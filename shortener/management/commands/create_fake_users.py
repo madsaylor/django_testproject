d9869e6ae46301b8e3ca773beaffd5eb14d9033b
Sergey Grigorchuk <grigochuk@gmail.com>
2016-04-19 10:51:24 +0300

    console command

---
+++ shortener/management/commands/create_fake_users.py
@@ -0,0 +1,43 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+from datetime import datetime
+import requests
+from pprint import pformat
+
+
+class Command(BaseCommand):
+    help = 'Generating fake users'
+
+    def add_arguments(self, parser):
+        parser.add_argument('number', nargs='+', type=int)
+
+    def handle(self, *args, **options):
+        include_fields = ','.join([
+            'name',
+            'email',
+            'registered',
+            'login'
+        ])
+
+        r = requests.get('http://api.randomuser.me', params={
+            'results': options['number'],
+            'inc': include_fields
+        })
+
+        # TODO: we can use bulk_update or DB transaction here
+        user_precount = User.objects.count()
+        for result in r.json()['results']:
+            u = User.objects.create_user(
+                result['login']['username'],
+                result['email'],
+                result['login']['password'],
+                first_name=result['name']['first'],
+                last_name=result['name']['last'],
+                date_joined=datetime.fromtimestamp(result['registered'])
+            )
+
+        user_diffcount = User.objects.count() - user_precount
+
+        self.stdout.write(self.style.SUCCESS(
+            'Successfully created {} users'.format(user_diffcount))
+        )
--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Link(models.Model):
+    url = models.URLField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    shortlink_path = models.CharField(max_length=1024)
 
 # Create your models here.
